7885eb85edfc1b882517b0862f0a226323ad4ebc
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2017-06-13 15:06:22 -0300

    Implementando Alert para quando o app não conseguir acessar a API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController , LoadingController } from 'ionic-angular';
+import { NavController , LoadingController , AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
@@ -13,7 +13,8 @@ export class HomePage {
   constructor(
     public navCtrl: NavController, 
     private _http : Http, 
-    private _loadingCtrl : LoadingController) {
+    private _loadingCtrl : LoadingController,
+    private _alertCtrl : AlertController) {
 
     let loader = this._loadingCtrl.create({
       content : 'Buscando novos carros. Aguarde ...'
@@ -26,11 +27,18 @@ export class HomePage {
       .map(res => res.json())
       .toPromise()
       .then(carros => {
-        loader.dismiss();
         this.carros = carros;
+        loader.dismiss();
+      }, err => {
+        console.log(err);
+        loader.dismiss();
+        this._alertCtrl
+          .create({
+            title : 'Falha na conexão',
+            buttons : [{ text : 'Estou ciente!'}],
+            subTitle : 'Não foi possível obter a lista de carros. Tente mais tarde.'
+          }).present();
       });
-
-
 
   }
 
