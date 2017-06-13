be7f10dc61d3f28dcf279b4df82bd03b03badcca
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2017-06-13 14:52:19 -0300

    Adicionando splash screen enquanto eh feita a requisicao http get

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController , LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
@@ -10,13 +10,28 @@ export class HomePage {
 
    public carros;
 
-  constructor(public navCtrl: NavController, private _http : Http) {
+  constructor(
+    public navCtrl: NavController, 
+    private _http : Http, 
+    private _loadingCtrl : LoadingController) {
+
+    let loader = this._loadingCtrl.create({
+      content : 'Buscando novos carros. Aguarde ...'
+    });
+
+    loader.present();
 
     this._http
       .get('http://aluracar.herokuapp.com/')
       .map(res => res.json())
       .toPromise()
-      .then(carros => this.carros = carros);
+      .then(carros => {
+        loader.dismiss();
+        this.carros = carros;
+      });
+
+
+
   }
 
 }
