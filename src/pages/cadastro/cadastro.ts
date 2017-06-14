20afb9ba1db1ced392bf3d3d2eea3074882121b3
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2017-06-14 18:03:29 -0300

    Seguindo com o cadastro de agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../domain/carro/carro';
+import { Http } from '@angular/http';
 
 @Component({
   templateUrl: 'cadastro.html'
@@ -15,7 +16,9 @@ export class CadastroPage {
   public email: string;
   public data: string = new Date().toISOString();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private _http: Http) {
 
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
@@ -23,11 +26,12 @@ export class CadastroPage {
   }
 
   agenda() {
-
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
+    
+    this._http
+            .get(`https://aluracar.herokuapp.com/salvarpedido?carro=${this.carro.nome}&nome=${this.nome}&preco=${this.precoTotal}&endereco=${this.endereco}&email=${this.email}&dataAgendamento=${this.data}`)
+            .toPromise()
+            .then(() => alert('Sucesso'))
+            .catch(erro => alert('Falha'));
 
   }
 
