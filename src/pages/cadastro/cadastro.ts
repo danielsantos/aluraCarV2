6a554684d457633a0654755dd2fbe63d800793e9
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2017-06-14 17:02:47 -0300

    Recebendo no back-end os dados do cadastro de agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -10,10 +10,24 @@ export class CadastroPage {
   public carro: Carro;
   public precoTotal: number;
 
+  public nome: string;
+  public endereco: string;
+  public email: string;
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
+
+  }
+
+  agenda() {
+
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
 
   }
 
