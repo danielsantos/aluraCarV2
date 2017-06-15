b1b72ee1dcf74bc286b4016b4e6eed9c9748043d
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-06-15 15:26:52 -0300

    Alerta do cadastro de agendamento

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../domain/carro/carro';
 import { Http } from '@angular/http';
 
@@ -16,12 +16,20 @@ export class CadastroPage {
   public email: string;
   public data: string = new Date().toISOString();
 
+  private _alert: Alert;
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private _http: Http) {
+              private _http: Http,
+              private _alertCtrl: AlertController) {
 
     this.carro = this.navParams.get('carro');
     this.precoTotal = this.navParams.get('precoTotal');
+    
+    this._alert = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons: [{ text: 'Ok'}]
+    });
 
   }
 
@@ -30,8 +38,14 @@ export class CadastroPage {
     this._http
             .get(`https://aluracar.herokuapp.com/salvarpedido?carro=${this.carro.nome}&nome=${this.nome}&preco=${this.precoTotal}&endereco=${this.endereco}&email=${this.email}&dataAgendamento=${this.data}`)
             .toPromise()
-            .then(() => alert('Sucesso'))
-            .catch(erro => alert('Falha'));
+            .then(() => {
+              this._alert.setSubTitle('Agendamento realizado com Sucesso!');
+              this._alert.present();
+            })
+            .catch(erro => {
+              this._alert.setSubTitle('Nao foi possivel realizar o agendamento');
+              this._alert.present();
+            });
 
   }
 
